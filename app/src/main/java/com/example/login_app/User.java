444352fb96d3b9a7cc3e6ba12c776945fbf7c2dd
OilPony <girl_oiljung@hotmail.com>
2019-02-18 18:59:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/login_app/User.java
@@ -0,0 +1,21 @@
+package com.example.login_app;
+
+/**
+ * Created by amardeep on 10/26/2017.
+ */
+
+public class User {
+    public String id;
+    public String userName;
+    public String email;
+    public String password;
+
+    public User(String id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
+
